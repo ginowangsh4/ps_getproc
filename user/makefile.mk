54bc9aff437c3847dea7bb0307ac413336abe9ed
ginowangsh4 <sihengwang2019@u.northwestern.edu>
2017-10-17 01:47:57 -0500

    work until 1:47

--- user/makefile.mk
+++ user/makefile.mk
@@ -15,9 +15,11 @@ USER_PROGS := \
 	stressfs\
 	tester\
 	usertests\
+	usertests_2_1\
 	wc\
 	zombie\
-	ps
+	ps\
+	derefnull
 
 USER_PROGS := $(addprefix user/, $(USER_PROGS))
 
@@ -74,7 +76,7 @@ USER_LDFLAGS += --omagic
 USER_LDFLAGS += --entry=main
 
 # location in memory where the program will be loaded
-USER_LDFLAGS += --section-start=.text=0x0
+USER_LDFLAGS += --section-start=.text=0x1000
 
 user/bin:
 	mkdir -p user/bin
